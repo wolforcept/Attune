7ca4a6cf7a86c6c3cc01ba9e9d04835564ad035f
wolforcept <miguelfeotorres@gmail.com>
2022-02-24 00:04:25 +0000

    Add project files.

---
+++ Attune.cs
@@ -0,0 +1,98 @@
+﻿using NAudio.Midi;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Attune {
+    public class Attune {
+
+        const bool debug = false;
+        static Form form;
+
+        [STAThread]
+        static void Main() {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            form = new Form(new Attune());
+            Application.Run(form);
+        }
+
+        private bool IsConnected => fire != null;
+        private AkaiFire fire = null;
+
+        internal void Start() {
+            Log("Connecting...");
+            if (!IsConnected) {
+                string reason; (fire, reason) = AkaiFire.Create(this);
+                Log(reason);
+                onStart();
+            } else
+                Log("Already Connected.");
+        }
+
+        internal void Close() {
+            fire.Close();
+            fire = null;
+        }
+
+        internal void OnMessage(MidiEvent e) {
+            if (debug) {
+                Log(DateTime.Now + ": ", e.GetAsShortMessage());
+            }
+
+            if (e is NoteEvent note) {
+                Log(note.CommandCode, note.NoteNumber);
+                if (note.NoteNumber >= 54 && note.NoteNumber <= 117) {
+                    int n = note.NoteNumber - 54;
+                    if (note is NoteOnEvent)
+                        OnPadPressed(n, n % 16, n / 16);
+                    else
+                        OnPadReleased(n, n % 16, n / 16);
+                }
+            }
+
+        }
+
+        internal void OnPadPressed(int n, int x, int y) {
+            fire.SetPadColor(x, y, 20 + n);
+        }
+
+        internal void OnPadReleased(int n, int x, int y) {
+            fire.SetPadColor(x, y, 0);
+        }
+
+        internal void OnError(object sender, MidiInMessageEventArgs args) {
+            if (debug) {
+                Log(DateTime.Now + ": ", args);
+            }
+            fire = null;
+        }
+
+        internal void OnSysex(object sender, MidiInSysexMessageEventArgs args) {
+            if (debug) {
+                Log(DateTime.Now + ": ", args);
+            }
+        }
+
+        internal void Log(params object[] objs) {
+            foreach (object s in objs)
+                form.AddText(s.ToString());
+            form.AddText("\n");
+        }
+
+        //
+        // PRIVATE METHODS
+        private void onStart() {
+            new Thread(new ThreadStart(AwakeCheck)).Start();
+        }
+
+        private void AwakeCheck() {
+            while (fire != null) {
+                fire.WriteOnScreen();
+                Thread.Sleep(1000);
+            }
+        }
+
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Attune {
+    partial class Form {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.startButton = new System.Windows.Forms.Button();
+            this.textbox = new System.Windows.Forms.RichTextBox();
+            this.clearConsoleButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.startButton.Location = new System.Drawing.Point(451, 12);
+            this.startButton.Name = "buttonStart";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.OnStart);
+            // 
+            // textbox
+            // 
+            this.textbox.BackColor = System.Drawing.Color.LightGray;
+            this.textbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textbox.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.textbox.Location = new System.Drawing.Point(12, 41);
+            this.textbox.Name = "textbox";
+            this.textbox.Size = new System.Drawing.Size(514, 234);
+            this.textbox.TabIndex = 3;
+            this.textbox.Text = "";
+            // 
+            // button1
+            // 
+            this.clearConsoleButton.Location = new System.Drawing.Point(370, 11);
+            this.clearConsoleButton.Name = "clearConsoleButton";
+            this.clearConsoleButton.Size = new System.Drawing.Size(75, 23);
+            this.clearConsoleButton.TabIndex = 4;
+            this.clearConsoleButton.Text = "Clear Console";
+            this.clearConsoleButton.UseVisualStyleBackColor = true;
+            this.clearConsoleButton.Click += new System.EventHandler(this.OnClear);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 287);
+            this.Controls.Add(this.clearConsoleButton);
+            this.Controls.Add(this.textbox);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form";
+            this.Text = "Attune";
+            this.FormClosing += OnFormClosing;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.RichTextBox textbox;
+        private System.Windows.Forms.Button clearConsoleButton;
+    }
+}
+
